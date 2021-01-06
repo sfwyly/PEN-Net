f79305c6a2c7fff425d8d11aeda550d17f350a39
sfwyly <31818508+sfwyly@users.noreply.github.com>
2021-01-06 22:25:35 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,20 +5,19 @@ def train_step(generator,discriminator, image_list, mask_image_list, mask_list):
         image_list = tf.cast(image_list, dtype=tf.float32)
         mask_image_list = tf.cast(mask_image_list, dtype=tf.float32)
         mask_list = tf.cast(mask_list, dtype=tf.float32)
-        pyramid_feats,gen_image_list = generator([mask_image_list, mask_list])
+        pyramid_feats,gen_image_list = generator([mask_image_list, 1.0 - mask_list])
         gen_image_list = tf.cast(gen_image_list, dtype=tf.float32)
         
         dis_real_feat = discriminator(image_list)
         dis_fake_feat = discriminator(gen_image_list)
-        dis_loss = (dis_real_feat + dis_fake_feat)/2.
+        dis_loss = (calv(dis_real_feat,1) + calv(dis_fake_feat,0))/2.
         
         for feat in pyramid_feats:
             pyramid_loss += l1_loss(feat,tf.image.resize(images,feat.shape[1:3],"bilinear"))
         
-        hole_loss = l1_loss(gen_image_list*mask_list, image_list*mask_list)/tf.reduce_mean(mask_list)
-        valid_loss = l1_loss(gen_image_list*(1-mask_list), image_list*(1-mask_list))/tf.reduce_mean(1-mask_list)
-        adv_feats = discriminator(gen_image_list)
-        adversarial_loss = calv(adv_feats,1)
+        hole_loss = l1_loss(gen_image_list*(1-mask_list), image_list*(1-mask_list))/tf.reduce_mean(1-mask_list)
+        valid_loss = l1_loss(gen_image_list*mask_list, image_list*mask_list)/tf.reduce_mean(mask_list)
+        adversarial_loss = calv(dis_fake_feat,1)
         gen_loss = hole_loss * 6. + valid_loss * 1. + pyramid_loss * 0.5 + adversarial_loss * 0.1
         
     generator_grads = tape.gradient(gen_loss, generator.trainable_variables)
